7a65ccb31129eb8eae6a1711a30c18c8e10d3138
ishikapandeyy <ishikapandey.workmail@gmail.com>
2026-09-23 12:15:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //your JS code here. If required.
-let para=document.querySelector("#status")
-let btn=document.querySelector("#enterBtn")
-btn.addEventListener("click",()=>{
-	para.innerHTML="<h1>Entered Metaverse</h1>"
-})
+let para = document.querySelector("#status");
+let btn = document.querySelector("#enterBtn");
+
+btn.addEventListener("click", () => {
+    let heading = document.createElement("h1");
+
+    heading.innerText = "Entered Metaverse";
+
+    para.replaceWith(heading);
+});
