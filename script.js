e4378661e743c3241cc599ccb1dcd570be7e9b84
ishikapandeyy <ishikapandey.workmail@gmail.com>
2026-09-23 11:54:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let para=document.querySelector("#status")
+let btn=document.querySelector("#enterBtn")
+btn.addEventListener("click",()=>{
+	para.innerHTML="<h1>Entered Metaverse</h1>"
+})
